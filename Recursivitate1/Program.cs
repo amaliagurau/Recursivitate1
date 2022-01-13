3f7939440ccc19cb582440ba01c513cb9c4b1513
Amalia Andreea Gurau <amalia.gurau@emmanuel.ro>
2022-01-13 15:41:16 +0200

    recursiv1

---
+++ Recursivitate1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Recursivitate1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] vector = new int[] { 5, 6, 7, 8, 9, 10 };
+            Recursiv.ParcurgereVector(vector, vector.Length);
+
+        }
+    }
+}
---
+++ Recursivitate1/Recursivitate.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Recursivitate1
+{
+    public static class Recursiv
+    {
+        public static void ParcurgereVector(int[] vector, int n)
+        {
+            if (n == 1)
+                Console.WriteLine(vector[n - 1]);
+            else
+            {
+
+                ParcurgereVector(vector, n - 1);
+                Console.WriteLine(vector[n - 1]);
+
+            }
+        }
+    }
+}
